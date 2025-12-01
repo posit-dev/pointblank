aadfe5d3884d1df87a0054ca6c2937ac96eee3f7
Tyler Riccio <tylerriccio8@gmail.com>
2025-12-01 14:10:13 -0500

    consolidate tests

--- tests/test_agg.py
+++ tests/test_agg.py
@@ -40,21 +40,6 @@ def test_sums_old(tol, simple_pl) -> None:
         ("col_sum_ge", (3, 6, 9)),
         ("col_sum_lt", (4, 7, 10)),
         ("col_sum_le", (3, 6, 9)),
-    ],
-)
-def test_sums(simple_pl: pl.DataFrame, method: str, vals: tuple[int, int, int]):
-    v = Validate(simple_pl)
-    for col, val in zip(["a", "b", "c"], vals):
-        getattr(v, method)(col, val)
-    v = v.interrogate()
-
-    v.assert_below_threshold()
-    v.get_tabular_report()
-
-
-@pytest.mark.parametrize(
-    ("method", "vals"),
-    [
         ("col_avg_eq", (1, 2, 3)),
         ("col_avg_gt", (0, 1, 2)),
         ("col_avg_ge", (1, 2, 3)),
@@ -62,7 +47,7 @@ def test_sums(simple_pl: pl.DataFrame, method: str, vals: tuple[int, int, int]):
         ("col_avg_le", (1, 2, 3)),
     ],
 )
-def test_avgs(simple_pl: pl.DataFrame, method: str, vals: tuple[int, int, int]):
+def test_aggs(simple_pl: pl.DataFrame, method: str, vals: tuple[int, int, int]):
     v = Validate(simple_pl)
     for col, val in zip(["a", "b", "c"], vals):
         getattr(v, method)(col, val)
