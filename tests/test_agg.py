3944b026a8713eacdb3dc72163026ab797e9173b
Tyler Riccio <tylerriccio8@gmail.com>
2025-12-01 13:34:56 -0500

    paramaterize tests

--- tests/test_agg.py
+++ tests/test_agg.py
@@ -4,6 +4,17 @@ from pointblank import Validate
 import polars as pl
 
 
+@pytest.fixture
+def simple_pl() -> pl.DataFrame:
+    return pl.DataFrame(
+        {
+            "a": [1, 1, 1, None],
+            "b": [2, 2, 2, None],
+            "c": [3, 3, 3, None],
+        }
+    )
+
+
 @pytest.mark.parametrize(
     "tol",
     [
@@ -13,12 +24,31 @@ import polars as pl
         0,
     ],
 )
-def test_sums(tol) -> None:
-    data = pl.DataFrame({"a": [1, 1, 1, None]})
-    v = Validate(data).col_sum_eq("a", 3).interrogate()
+def test_sums_old(tol, simple_pl) -> None:
+    v = Validate(simple_pl).col_sum_eq("a", 3, tol=tol).interrogate()
 
     v.assert_below_threshold()
 
+    v.get_tabular_report()
+
+
+@pytest.mark.parametrize(
+    ("method", "vals"),
+    [
+        ("col_sum_eq", (3, 6, 9)),
+        ("col_sum_gt", (2, 5, 8)),
+        ("col_sum_ge", (3, 6, 9)),
+        ("col_sum_lt", (4, 7, 10)),
+        ("col_sum_le", (3, 6, 9)),
+    ],
+)
+def test_sums(simple_pl: pl.DataFrame, method: str, vals: tuple[int, int, int]):
+    v = Validate(simple_pl)
+    for col, val in zip(["a", "b", "c"], vals):
+        getattr(v, method)(col, val)
+    v = v.interrogate()
+
+    v.assert_below_threshold()
     v.get_tabular_report()
 
 
