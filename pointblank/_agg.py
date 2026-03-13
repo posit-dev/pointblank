9e3f3f10b5ca6336c5c1c682b5a9e497d92db1c9
Tyler Riccio <tylerriccio8@gmail.com>
2026-03-13 11:21:22 -0400

    check internal ordering more explicitly

--- pointblank/_agg.py
+++ pointblank/_agg.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 import itertools
 from collections.abc import Callable
-from typing import Any
 
 import narwhals as nw
 
-Aggregator = Callable[[nw.DataFrame | nw.LazyFrame], float | int]
-Comparator = Callable[[Any, Any, Any], bool]
+from pointblank._typing import SupportsOrder, supports_order
+
+Aggregator = Callable[[nw.DataFrame | nw.LazyFrame], SupportsOrder]
+Comparator = Callable[[SupportsOrder, SupportsOrder, SupportsOrder], bool]
 
 AGGREGATOR_REGISTRY: dict[str, Aggregator] = {}
 
@@ -28,58 +29,58 @@ def register(fn):
 
 ## Aggregator Functions
 @register
-def agg_sum(column: nw.DataFrame | nw.LazyFrame) -> float:
+def agg_sum(column: nw.DataFrame | nw.LazyFrame) -> SupportsOrder:
     plan = column.select(nw.all().sum())
     result = plan.collect().item() if isinstance(plan, nw.LazyFrame) else plan.item()
-    assert isinstance(result, (int, float))
+    assert supports_order(result), "INTERNAL: Query scalar would not support ordering."
     return result
 
 
 @register
-def agg_avg(column: nw.DataFrame | nw.LazyFrame) -> float:
+def agg_avg(column: nw.DataFrame | nw.LazyFrame) -> SupportsOrder:
     plan = column.select(nw.all().mean())
     result = plan.collect().item() if isinstance(plan, nw.LazyFrame) else plan.item()
-    assert isinstance(result, (int, float))
+    assert supports_order(result), "INTERNAL: Query scalar would not support ordering."
     return result
 
 
 @register
-def agg_sd(column: nw.DataFrame | nw.LazyFrame) -> float:
+def agg_sd(column: nw.DataFrame | nw.LazyFrame) -> SupportsOrder:
     plan = column.select(nw.all().std())
     result = plan.collect().item() if isinstance(plan, nw.LazyFrame) else plan.item()
-    assert isinstance(result, (int, float))
+    assert supports_order(result), "INTERNAL: Query scalar would not support ordering."
     return result
 
 
 ## Comparator functions:
 @register
-def comp_eq(real: float, lower: float, upper: float) -> bool:
+def comp_eq(real: SupportsOrder, lower: SupportsOrder, upper: SupportsOrder) -> bool:
     if lower == upper:
         return bool(real == lower)
     return _generic_between(real, lower, upper)
 
 
 @register
-def comp_gt(real: float, lower: float, upper: float) -> bool:
+def comp_gt(real: SupportsOrder, lower: SupportsOrder, upper: SupportsOrder) -> bool:
     return bool(real > lower)
 
 
 @register
-def comp_ge(real: Any, lower: float, upper: float) -> bool:
+def comp_ge(real: SupportsOrder, lower: SupportsOrder, upper: SupportsOrder) -> bool:
     return bool(real >= lower)
 
 
 @register
-def comp_lt(real: float, lower: float, upper: float) -> bool:
+def comp_lt(real: SupportsOrder, lower: SupportsOrder, upper: SupportsOrder) -> bool:
     return bool(real < upper)
 
 
 @register
-def comp_le(real: float, lower: float, upper: float) -> bool:
+def comp_le(real: SupportsOrder, lower: SupportsOrder, upper: SupportsOrder) -> bool:
     return bool(real <= upper)
 
 
-def _generic_between(real: Any, lower: Any, upper: Any) -> bool:
+def _generic_between(real: SupportsOrder, lower: SupportsOrder, upper: SupportsOrder) -> bool:
     """Call if comparator needs to check between two values."""
     return bool(lower <= real <= upper)
 
--- pointblank/_typing.py
+++ pointblank/_typing.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import datetime
 import sys
 from collections.abc import Container
-from typing import List, Tuple, Union
+from typing import List, Protocol, Tuple, TypeGuard, Union
 
 # Check Python version for TypeAlias support
 if sys.version_info >= (3, 10):
@@ -75,3 +75,20 @@ try:
     )
 except AttributeError:
     pass
+
+
+# Need this to describe a generic numeric type that we can compare against another generic type
+# ie ordering. Apparently, python never implemented this which surprised me. There is numeric.Real
+# but that is a little narrower and wishy washy w/some numpy or FFI driven types. So, I guess we'll
+# define a little protocol here to help us. Also surprising is we don't have a super reliable
+# way to enforce this without a type guard, so we define `supports_order`. I originally thought this
+# was overkill but it's used in a lot of internal agg code, and a failure downstream would be
+# very confusing. I think this fits a case where an AssertionError is enough to qualify the runtime
+# check.
+class SupportsOrder(Protocol):
+    def __lt__(self, other: object) -> bool: ...
+    def __gt__(self, other: object) -> bool: ...
+
+
+def supports_order(x: object) -> TypeGuard[SupportsOrder]:
+    return hasattr(type(x), "__lt__") and hasattr(type(x), "__gt__")
