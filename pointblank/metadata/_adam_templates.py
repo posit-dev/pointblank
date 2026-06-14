910f6f42b681e098f8a74c5a8ad7039d0f40dbe0
Richard Iannone <riannone@me.com>
2026-06-14 10:52:14 -0400

    Add ADaM templates module

---
+++ pointblank/metadata/_adam_templates.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from dataclasses import field as dataclass_field
+from typing import Any
+
+__all__ = [
+    "ADaMDatasetTemplate",
+    "ADaMVariableSpec",
+    "get_adam_dataset",
+    "list_adam_datasets",
+    "validate_adam_structure",
+]
+
+
